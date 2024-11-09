5061acd37d8da02cde8821692ba68adc94472efd
Faiz <faiz199011@gmail.com>
2024-11-09 11:44:24 +0430

    refactor: onboarding screen

--- lib/src/featured/auth/widgets/do_not_have_account.dart
+++ lib/src/featured/auth/widgets/do_not_have_account.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import '../../../route/routes.dart';
 
 import '../../../helper/constants/app_colors.dart';
 
@@ -15,24 +12,27 @@ class DoNotHaveAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Don’t have an account?', style: CustomTextStyle.semiBold(
-          size: 16,
-          color: Colors.white,
-          fontWeight: FontWeight.w600
-        ),),
-
-        CustomTextButton(
-          child: Text('Sign up', style: CustomTextStyle.regular(
+    return Padding(
+      padding: const EdgeInsets.only(top: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Don’t have an account?', style: CustomTextStyle.semiBold(
             size: 16,
-            color: AppColors.primaryColor,
+            color: Colors.white,
             fontWeight: FontWeight.w600
           ),),
-          onPressed: (){}
-        )
-      ],
+
+          CustomTextButton(
+            child: Text('Sign up', style: CustomTextStyle.regular(
+              size: 16,
+              color: AppColors.primaryColor,
+              fontWeight: FontWeight.w600
+            ),),
+            onPressed: (){}
+          )
+        ],
+      ),
     );
   }
 }
